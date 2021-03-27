bc16acdcd003259e567b292b0fceb09c1fc0757b
James Posey <jamesrposeyjr@gmail.com>
2021-03-26 19:34:22 -0500

    trying to fix url issue on heroku

--- server/server.js
+++ server/server.js
@@ -44,7 +44,6 @@ app.use('/api/babyguess', babyGuessRouter);
 app.use('/api/scores', scoreRouter);
 
 // Serve static files
-app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
