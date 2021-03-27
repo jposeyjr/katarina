e81834a4238a4ba520be4907c5625c829a332944
James Posey <jamesrposeyjr@gmail.com>
2021-03-26 20:52:08 -0500

    changed folder dir for build

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ app.use('/api/scores', scoreRouter);
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+  res.sendFile(path.join(__dirname, '.build/index.html'));
 });
 
 // App Set //
